070ea11860fbd71bc5fa37831db3d04e7002591d
Maher Ahmed <117451247+MaherAhmed9@users.noreply.github.com>
2025-04-18 18:28:17 +0200

    add theme switcher

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ window.addEventListener('load', function() {
         window.addEventListener('load', hideLoader);
     }
 });
+
+function toggleTheme() {
+    const html = document.documentElement;
+    const currentTheme = html.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    
+    html.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    document.documentElement.setAttribute('data-theme', savedTheme);
+});
